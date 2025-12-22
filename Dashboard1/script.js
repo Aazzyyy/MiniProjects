5b90f4ad2c6e99fd24361779e647d4f7d5e799eb
Ashmit aka Azzy <207732305+Aazzyyy@users.noreply.github.com>
2025-12-22 18:08:36 +0530

    abit more style and back function added

--- Dashboard1/script.js
+++ Dashboard1/script.js
@@ -1,8 +1,19 @@
-var elements = document.querySelectorAll('.elems');
-var Expand = document.querySelectorAll('.elem_exp_view');
+function ExpandViewOPenCLose() {
+    var elements = document.querySelectorAll('.elems');
+    var Expand = document.querySelectorAll('.elem_exp_view');
+    var backButton = document.querySelectorAll('.back');
 
-elements.forEach(function(element, index){
-    element.addEventListener('click', function(){
-        Expand[index].style.display = 'block';
+    elements.forEach(function (element, index) {
+        element.addEventListener('click', function () {
+            Expand[index].style.display = 'block';
+        })
     })
-})
+
+    backButton.forEach(function (button, index) {
+        button.addEventListener('click', function () {
+            Expand[index].style.display = 'none';
+        })
+    })
+}
+
+ExpandViewOPenCLose();
